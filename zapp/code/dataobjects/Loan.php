053be3072c9efd4d12d5af9fa5a2340efb6a4795
maent45 <maent45@gmail.com>
2017-11-30 12:12:52 +1300

    fleshing out model

--- zapp/code/dataobjects/Loan.php
+++ zapp/code/dataobjects/Loan.php
@@ -1,2 +1,36 @@
 <?php
 
+namespace mt2;
+
+use SilverStripe\Assets\File;
+use SilverStripe\ORM\DataObject;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\TextField;
+use SilverStripe\AssetAdmin\Forms\UploadField;
+use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
+
+class Loan extends DataObject {
+    
+  private static $db = [
+    'Loanee' => 'Varchar',
+    'Amount' => 'Varchar',
+    'StartDate' => 'Varchar',
+    'EndDate' => 'Varchar'
+  ];
+  
+  public function getCMSFields() {
+
+    $fields = parent::getCMSFields();
+
+    $fields->addFieldsToTab('Root.Main', array(
+      TextField::create('Loanee'),
+      TextField::create('Amount'),
+      TextField::create('StartDate'),
+      TextField::create('EndDate')
+    ));
+
+    return $fields;
+
+  }
+  
+}
